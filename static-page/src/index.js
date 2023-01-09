0b9c729ea6f2c1049039a7cc287982d7253c6304
Hetali Shah Gala <hetali.shah.gala@gmail.com>
2023-01-09 15:36:37 -0500

    parent-children components and styling

--- static-page/src/index.js
+++ static-page/src/index.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
 import logo from './logo.svg';
+import './index.css';
 
 function Page() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src={logo} width="50px"></img>
-        </nav>
-      </header>
-      <h1>Reasons I'm excited to learn React</h1>
+      <Header />  
+      <MainContent />
+      <Footer />
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <header>
+      <nav className="navBar">
+        <img className="logoSVG" src={logo} alt=""></img>
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <small> © 2023 Hetali Shah development. All rights reserved. </small>
+    </footer>
+  )
+}
+
+function MainContent() {
+  return (
+    <div>
+      <h1 className="title">Reasons I'm excited to learn React</h1>
       <ol>
         <li>Single-page application</li>
         <li>Popular library</li>
         <li>Declarative library</li>
       </ol>
-      <footer>
-        <small> © 2023 Hetali Shah development. All rights reserved. </small>
-      </footer>
     </div>
   )
 }
