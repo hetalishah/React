afc87f923937d39fed6e6c3b0fdec135c4c3b62d
Hetali Shah Gala <hetali.shah.gala@gmail.com>
2023-01-11 17:02:06 -0500

    renaming & reorganization

---
+++ practice-projects/practice6/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header/header.js';
+import MainContent from './MainContent/main-content.js';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+    </div>
+  );
+}
